32905c34fa14034fa4bf31fe1790818b234b704b
lee mighdoll <lee@underneath.ca>
2025-07-30 13:48:31 -0700

    simplify with util

--- src/app/JustReduce.ts
+++ src/app/JustReduce.ts
@@ -1,19 +1,16 @@
-import { bufferF32 } from "thimbleberry";
 import { reduceBuffer } from "../reduce/ReduceBuffer.ts";
-import { requestGpuDevice } from "./GpuUtil.ts";
+import { randomBufferF32, requestGpuDevice } from "./GpuUtil.ts";
 
-/** importing from WGSL/WESL to typescript via reflection could look like this:
-import { sumF32 } from "../reduce/shaders/binOps.wesl";
-*/
-
-const { floor, random } = Math;
+/* importing from WGSL/WESL to typescript via reflection could look like this: */
+// import { sumF32 } from "../reduce/shaders/binOps.wesl";
 const sumF32 = null as any;
 
-/** sample app that sums a buffer full of f32s on the gpu */
+/** sample app that sums a buffer full of f32s on the gpu.
+ * Simple use of a hosted shader.
+ */
 export async function appReduce(): Promise<void> {
   const gpuDevice = await requestGpuDevice();
-  const randomData = new Array(1024).fill(0).map(() => floor(random() * 100));
-  const buffer: GPUBuffer = bufferF32(gpuDevice, randomData);
+  const buffer: GPUBuffer = randomBufferF32(gpuDevice, 1024);
 
   const rb = reduceBuffer({ gpuDevice, inputBuffer: buffer, binOp: sumF32 });
   const value = rb.reduce();
